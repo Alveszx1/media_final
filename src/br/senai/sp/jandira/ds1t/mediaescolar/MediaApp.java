be5debbf39c7cd252e967d032b23bbf1376c6fcb
BRUNO HADDAD ALVES <25203660@CFP127ALUNO.LOCAL>
2025-09-22 14:18:22 -0300

    Projeto de média escolar usando condicionais

---
+++ src/br/senai/sp/jandira/ds1t/mediaescolar/MediaApp.java
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.ds1t.mediaescolar;
+
+import br.senai.sp.jandira.ds1t.mediaescolar.model.ResultadoFinal;
+
+public class MediaApp {
+    public static void main(String[] args) {
+        System.out.println("*************** CÁLCULO DE MEDIA ESCOLAR ***************");
+        ResultadoFinal resultado = new ResultadoFinal();
+        resultado.coletarDados();
+
+
+
+    }
+}
---
+++ src/br/senai/sp/jandira/ds1t/mediaescolar/model/ResultadoFinal.java
@@ -0,0 +1,53 @@
+package br.senai.sp.jandira.ds1t.mediaescolar.model;
+
+import java.util.Scanner;
+
+public class ResultadoFinal {
+    String nomeAluno;
+    double nota1;
+    double nota2;
+    double nota3;
+    double nota4;
+    double mediaFinalAluno;
+    String aprovadoReprovado;
+
+    public void coletarDados(){
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        System.out.print("Digite o nome do aluno: ");
+        nomeAluno = leitor.nextLine();
+
+        System.out.print("Digite a primeira nota: ");
+        nota1 = leitor.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        nota2 = leitor.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        nota3 = leitor.nextDouble();
+
+        System.out.print("Digite a quarta nota: ");
+        nota4 = leitor.nextDouble();
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        calcularMedia();
+    }
+    public void calcularMedia(){
+        mediaFinalAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (mediaFinalAluno >= 5){
+            aprovadoReprovado = "Aprovado";
+        }else{
+            aprovadoReprovado = "Reprovado";
+        }
+        exibirResultados();
+    }
+    public void exibirResultados(){
+        System.out.println("Média do aluno: "+mediaFinalAluno);
+        System.out.print("Resultado: O(a) aluno(a) "+nomeAluno+ " esta "+aprovadoReprovado);
+    }
+}
